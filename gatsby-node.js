bd5db730a19ed7cad74d06432ec3589c87660439
rishi-chethan <sevakula.rishichaithan@gmail..com>
2020-12-28 18:51:41 +0530

    changed async/await

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,17 +2,14 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 exports.createPages = async ({ actions }) => {
-    
-    const charTemplate = path.resolve(`${__dirname}/src/templates/Character.js`)
-
-    await fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(res => {
-            res.results.forEach(char => {
-            actions.createPage ({
-                path:`/character/${char.id}`,
-                component: charTemplate,
-                context: char
-            });
-        });
+  const charTemplate = path.resolve(`${__dirname}/src/templates/Character.js`)
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const data = await res.json()
+  data.results.forEach(char => {
+    actions.createPage({
+      path: `/character/${char.id}`,
+      component: charTemplate,
+      context: char
     });
-    
+  });
 }
